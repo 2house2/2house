6992aef6e539fc72247c92f2bbae6ac9c2b6949c
Vanessa Zlatar <tu_correo@gmail.com>
2025-05-12 19:27:48 -0400

    Vista de búsqueda lista

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,26 +1,22 @@
 class PropertiesController < ApplicationController
   before_action :authenticate_user!
 
-  def index
-    @properties = current_user.properties
-  end
-
-  def new
-    @property = Property.new
-  end
-
   def create
-    @property = current_user.properties.build(property_params)
+    @property = current_user.properties.new(property_params)
     if @property.save
-      redirect_to properties_path, notice: 'Propiedad creada exitosamente.'
+      redirect_to properties_path
     else
       render :new
     end
   end
 
+  def search
+    @properties = Property.all
+  end
+
   private
 
   def property_params
-    params.require(:property).permit(:title, :description, :price, :address)
+    params.require(:property).permit(:title, :location, :available_from, :available_until, :max_guests)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   get "home/index"
   devise_for :users
   resources :properties
+  get "search", to: "properties#search", as: "search_properties"
   root "home#index"
 end
